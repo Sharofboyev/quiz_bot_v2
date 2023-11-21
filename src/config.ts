5c664aa6fc16ce97d3a41ce87333329b95b21743
Sarvar Sharofboyev <sarvar.sh@payme.uz>
2023-11-21 19:07:14 +0500

    Initial commit

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+import { config } from "dotenv";
+config();
+
+export default {
+    token: ,
+    DB: {
+        host: "91.203.192.50",
+        database: "gamer",
+        port: 5432,
+        password: "Knyaz2000",
+        user: "postgres",
+    },
+    test_pay: "284685063:TEST:YzI3ZDE4NjgxZDE5",
+    live_pay: "350862534:LIVE:ZTU4NDAxMTEyNTkw",
+    pic_dice:
+        "https://www.boardgamesmaker.com/AttachFiles/WebsiteImages/Product_ItemBig/FI_8321.jpg",
+    time_out: 12,
+    free_time: 120,
+    free_pay1: 3000,
+    free_pay2: 1000,
+    free_pay3: 600,
+    owner: 373256429,
+    end_game_photo:
+        "AgACAgIAAxkBAAINM2DhogbBIr4Z880iUfWejSIvvSNiAAKTsjEbSFMISxRHygxIqW4eAQADAgADcwADIAQ",
+    decreased_energy_photo:
+        "AgACAgIAAxkBAAIOaGED-V0RSaYXSb_RDV2C9WTfCroaAAK1sTEbnqggSA8baPLrClf1AQADAgADcwADIAQ",
+};
